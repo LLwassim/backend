c35b4cdf3022c408ab50e777daa069f0ca7bd830
LLwassim <lacorchy77@gmail.com>
2023-10-18 13:04:50 -0400

    Seperated Back End for deployment

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database.js");
+
+const User = sequelize.define(
+  "User",
+  {
+    customer_id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      unique: true,
+    },
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    date_of_birth: DataTypes.DATE,
+    contact_number: DataTypes.STRING,
+    address: DataTypes.TEXT,
+    account_type: DataTypes.STRING,
+    created_at: DataTypes.DATE,
+  },
+  {
+    tableName: "users", // Specify the table name here
+    timestamps: false,
+  }
+);
+
+module.exports = User;
