bd18e3710c38bf9530bd3ae1949e5ef9079730af
LLwassim <lacorchy77@gmail.com>
2023-10-20 17:02:19 -0400

    Updated CORS to work in production env

--- server.js
+++ server.js
@@ -5,26 +5,28 @@ const port = 5001;
 const cors = require("cors");
 require("dotenv").config();
 app.use(express.json());
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "https://llwassim.github.io/bankingapp-frontend",
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Required for handling cookies, authentication, or sessions
+  allowedHeaders: ["Content-Type", "Authorization"], // Specify the headers your server expects
+};
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://llwassim.github.io/bankingapp-frontend",
-    ],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // Required for handling cookies, authentication, or sessions
-  })
-);
+app.use(cors(corsOptions));
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
 });
+
 const usersRouter = require("./routes/users");
 
 app.use("/api/users", usersRouter);
+app.options("/api/users/signup", cors(corsOptions)); // Handle pre-flight for this specific route
 
 db.connect((err) => {
   if (err) {
